006eb10e9d28d0abe6e7bb69d19da9ad20a4f705
ZUEB001 <raphael.zueblin@sonymusic.com>
2020-04-23 10:21:50 +0200

    Updates

---
+++ precache-manifest.d7b29a28b565e064cfe4117ba71134b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223a015eb170bc02ba7b8d819493fbd5",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "d8dd2663f6d08292464c",
+    "url": "/react-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0413c559a72e55e4719f",
+    "url": "/react-deploy/static/js/2.6e74cc94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.6e74cc94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8dd2663f6d08292464c",
+    "url": "/react-deploy/static/js/main.8ee18d63.chunk.js"
+  },
+  {
+    "revision": "ab96405cd50425e31642",
+    "url": "/react-deploy/static/js/runtime-main.2434a08a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
